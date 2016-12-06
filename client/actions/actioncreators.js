220635bffe9d65ee7c37aa86b3786ce57d0571fb
Sean Kochel <seankochel@gmail.com>
2016-12-05 20:23:56 -0500

    changed to secure https

--- client/actions/actioncreators.js
+++ client/actions/actioncreators.js
@@ -55,7 +55,7 @@ export function getData(city) {
 
 	return dispatch => {
 		dispatch(requestData(city));
-		fetchJsonp('http://api.wunderground.com/api/b177596eb43ca2fe/forecast10day/q/zmw:'+city+'.json')
+		fetchJsonp('https://api.wunderground.com/api/b177596eb43ca2fe/forecast10day/q/zmw:'+city+'.json')
 		.then(response=> {return response.json()})
 		.then(json=>dispatch(receiveData(city, json)))
 	}
@@ -67,7 +67,7 @@ export function getConditions(city) {
 	return dispatch => {
 		dispatch(requestDataConditions(city));
 		console.log("got request data conditions dispatch");
-		fetchJsonp('http://api.wunderground.com/api/b177596eb43ca2fe/conditions/q/zmw:'+city+'.json')
+		fetchJsonp('https://api.wunderground.com/api/b177596eb43ca2fe/conditions/q/zmw:'+city+'.json')
 		.then(response=>{return response.json()})
 		.then(json=>dispatch(receiveDataConditions(city, json)))
 		console.log("got to end of dispatch");
@@ -78,7 +78,7 @@ export function getMultiDay(city) {
 
 	return dispatch => {
 		dispatch(requestMultiData(city));
-		fetchJsonp('http://api.wunderground.com/api/b177596eb43ca2fe/forecast10day/q/zmw:'+city+'.json')
+		fetchJsonp('https://api.wunderground.com/api/b177596eb43ca2fe/forecast10day/q/zmw:'+city+'.json')
 		.then(response=> {return response.json()})
 		.then(json=>dispatch(receiveMultiData(city, json)))
 	}
